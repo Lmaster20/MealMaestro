a826ba488a483695fbb9c865c40fe658a6f3938d
Lmaster20 <rjones014@hotmail.com>
2018-03-24 01:46:13 -0400

    django button test 3.1

--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -8,4 +8,17 @@ class HomeView(TemplateView):
 def submit(request):
     info=request.POST['info']
     # do something with info
-    print("yay")
+    if test_string == "good":
+        return HttpResponse("Go to bed")
+    else:
+        return HttpResponse("Fuck")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,10 +16,21 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from catalog.views import HomeView
+from django.urls import path
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view()),
     url(r'^submit', views.submit),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
 
 ]
