4d6b7570146e9c6d761858c4f85fc54d0f7b8f7a
Lmaster20 <rjones014@hotmail.com>
2018-03-23 23:01:28 -0400

    basic django/mysql integration

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'TribeHacksRyan$default',
+        'USER': 'TribeHacksRyan',
+        'PASSWORD': 'chachasforlife',
+        'HOST': 'TribeHacksRyan.mysql.pythonanywhere-services.com',
     }
 }
 
